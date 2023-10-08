dae2befb18694c8fe7e49a6812b48c3d2c4bb8ba
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-10-08 13:51:01 -0700

    Passed all tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
-// create your App component here
+import { useEffect, useState } from "react";
+import DogImg from "./DogImg";
+
+const App = () =>{
+    const [dogImg,setDogImg] = useState(null)
+
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(resp => resp.json())
+        .then(data => setDogImg(data.message))
+    },[])
+
+    return(
+        dogImg ? <DogImg src={dogImg}/> : <p>Loading...</p>
+    )
+
+}
+
+export default App;
---
+++ src/components/DogImg.js
@@ -0,0 +1,9 @@
+const DogImg = ({src}) =>{
+    return(
+        <div>
+            <img src={src} alt="A Random Dog"/>
+        </div>
+    )
+}
+
+export default DogImg;
